8e9b3b10a938ded34271e68671d57dd542ef8443
Marco Cabrera <52000890+marcocabrerahdez@users.noreply.github.com>
2022-11-16 19:37:14 +0000

    [#183818524] Testing with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  test: {
+    /*
+      * The default test runner is jest, but you can use any test runner you want.
+      * For example, you can use mocha, ava, or even your own runner.
+    */
+   }
+});
