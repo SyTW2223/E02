a07ccd3254a11d7016b16fc1612f8ffa9e8403c9
Joseph Gabino Rodríguez <72351056+alu0101329161@users.noreply.github.com>
2022-11-17 14:53:59 +0000

    [#183818686] - Implemented Redux

---
+++ src/features/auth/auth.ts
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const auth = createSlice({
+    name: "auth",
+    initialState: [],
+    reducers: {},
+});
+
+
+export default auth.reducer;
