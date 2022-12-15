c918b94532de394196def45a42be5e87f59552e2
Marco Cabrera <marco.cabrerahdez@gmail.com>
2022-12-15 16:43:00 +0000

    Update vite.config.ts

--- Front-end/vite.config.ts
+++ Front-end/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react()],
   define: {
     'process.env.BACK_HOST': process.env.BACK_HOST || `http://localhost:3000`,
   },
