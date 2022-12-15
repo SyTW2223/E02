d28d2d5358fd3a9ffaa7245b9ac2304079ae2204
Marco Cabrera <marco.cabrerahdez@gmail.com>
2022-12-15 16:40:34 +0000

    [#184016344]-deployment-bug

--- Front-end/vite.config.ts
+++ Front-end/vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),],
+  define: {
+    'process.env.BACK_HOST': process.env.BACK_HOST || `http://localhost:3000`,
+  },
   base: './',
   test: {
     globals: true,
