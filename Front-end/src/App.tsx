ba1591e47f87a835ad21416cc0ce9c803a2615bb
Joseph Gabino Rodríguez <72351056+alu0101329161@users.noreply.github.com>
2022-12-26 19:42:29 +0000

    Add Protedted Routed

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -7,27 +7,37 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import Home from './components/Home'
 import Layout from './components/Layout';
 import Login from './components/Login';
+import { ProtectedRoute } from './components/ProtectedRoute';
 import ProfilePageGucci from './components/ProfileGucci';
 import Direccion from './components/Direccion';
 import Tienda from './components/Tienda/Tienda';
-
 import Error from './components/Error';
 import PanData from './components/PanData';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const value = useSelector((state: any) => state.auth)
-  console.log(value);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/profile' element={<ProfilePageGucci />} />
-        <Route path='/direccion' element={<Direccion />} />
-        <Route path='/tienda' element={<Tienda />} />
-        <Route path='/pan/:id' element={<PanData />} />
+          <Route index element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/profile' element={
+            <ProtectedRoute>
+              <ProfilePageGucci />
+            </ProtectedRoute>
+          } />
+          <Route path='/direccion' element={<Direccion />} />
+          <Route path='/tienda' element={
+            <ProtectedRoute>
+              <Tienda />
+            </ProtectedRoute>
+          } />
+          <Route path='/pan/:id' element={
+            <ProtectedRoute>
+              <PanData />
+            </ProtectedRoute>
+          } />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
