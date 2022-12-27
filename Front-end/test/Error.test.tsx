c0d54e90011a28dc369e5ac56ea062d71233fcdb
Joseph Gabino Rodríguez <72351056+alu0101329161@users.noreply.github.com>
2022-12-27 15:54:53 +0000

    Fix test

--- Front-end/test/Error.test.tsx
+++ Front-end/test/Error.test.tsx
@@ -5,7 +5,7 @@ import Error from '../src/components/Error'
 
 describe('Error componente', () => {
   test('El componente se renderiza correctamente', () => {
-    const { getByRole } = renderWithProviders(<Error />)
-    expect(getByRole('heading')).toBeTruthy()
+    const { getByText } = renderWithProviders(<Error />)
+    expect(getByText('404')).toBeTruthy()
   })
 })
