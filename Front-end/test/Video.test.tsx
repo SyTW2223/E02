7b14e193b675892835e35e60c4ade24e0ad9c348
Joseph Gabino Rodríguez <72351056+alu0101329161@users.noreply.github.com>
2022-12-27 10:43:51 +0000

    Fix test

--- Front-end/test/Video.test.tsx
+++ Front-end/test/Video.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { describe, expect, test } from 'vitest'
 import { renderWithProviders } from '../src/utils/test-utils'
-import Video from '../src/components/Video'
+import Video from '../src/components/Home/Video'
 
 describe('Video componente', () => {
   test('El componente se renderiza correctamente', () => {
